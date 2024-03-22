26ff4d2ff3aa6df78e9555ab32f96d0cdf3d3414
sureshit13 <160747364+sureshit13@users.noreply.github.com>
2024-03-22 16:36:00 +0530

    Update cf7-mail-testing-plugin.php

--- cf7-mail-testing-plugin.php
+++ cf7-mail-testing-plugin.php
@@ -3,10 +3,10 @@
 Plugin Name: Cf7 Mail Testing Plugin
 Plugin URI: https://github.com/sureshit13/Cf7-Mail-Testing-Plugin.git
 Description: This plugin skips sending emails for Contact Form 7 submissions and sends the email to a specified custom email address.
-Version: 1.2
+Version: 1.4
 Author: Suresh Dutt
 */
-require 'plugin-update-checker/plugin-update-checker.php';
+require_once 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 $myUpdateChecker = PucFactory::buildUpdateChecker(
 	'https://github.com/sureshit13/Cf7-Mail-Testing-Plugin/',
